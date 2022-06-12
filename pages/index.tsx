093468b4f224e4432e6e4a454c4f1de2dd23963d
Bender-22 <85234598+Bender-22@users.noreply.github.com>
2022-06-12 11:32:52 +0800

    Adjust CSS

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-gray-50">
       <Head>
         <title>Liquidity Provider Rewards | elimu.ai</title>
         <link rel="icon" href="/favicon.ico" />
@@ -21,40 +21,57 @@ export default function Home() {
         </p>
 
         <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a href="/uniswap" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-purple-600 focus:text-purple-600">
-            <h3 className="text-2xl font-bold">Uniswap Liquidity Pool 🦄</h3>
-            <p className="mt-4 text-xl">
-              <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ELIMU/$ETH 50%/50%</code>
-            </p>
-            <p className="mt-4">
-              APY: 0.00%
-            </p>
-          </a>
+          <div className="bg-white p-6 mt-6 border w-96 rounded-2xl drop-shadow-md">
+            <a href="/uniswap" className="hover:text-purple-600 focus:text-purple-600">
+              <h3 className="text-2xl font-bold">Uniswap Liquidity Pool 🦄</h3>
+              <p className="mt-4 text-xl">
+                <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ELIMU/$ETH 50%/50%</code>
+              </p>
+              <p className="mt-4">
+                Token emissions: 0.00 <code className="font-mono">$ELIMU</code>/day
+              </p>
+              <p className="mt-2">
+                TVL: 0.00 <code className="font-mono">$ETH</code> &nbsp;&nbsp;&nbsp; APY: 0.00%
+              </p>
+            </a>
+          </div>
 
-          <a href="/sushiswap" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-purple-600 focus:text-purple-600">
-            <h3 className="text-2xl font-bold">SushiSwap Liquidity Pool 🍣</h3>
-            <p className="mt-4 text-xl">
-              <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ELIMU/$ETH 50%/50%</code>
-            </p>
-            <p className="mt-4">
-              APY: 0.00%
-            </p>
-          </a>
+          <div className="bg-white p-6 mt-6 border w-96 rounded-2xl drop-shadow-md">
+            <a href="/sushiswap" className="hover:text-purple-600 focus:text-purple-600">
+              <h3 className="text-2xl font-bold">SushiSwap Liquidity Pool 🍣</h3>
+              <p className="mt-4 text-xl">
+                <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ELIMU/$ETH 50%/50%</code>
+              </p>
+              <p className="mt-4">
+                Token emissions: 0.00 <code className="font-mono">$ELIMU</code>/day
+              </p>
+              <p className="mt-2">
+                TVL: 0.00 <code className="font-mono">$ETH</code> &nbsp;&nbsp;&nbsp; APY: 0.00%
+              </p>
+            </a>
+          </div>
+          
+          <div className="bg-white p-6 mt-6 border w-96 rounded-2xl drop-shadow-md">
+            <a href="/balancer" className="hover:text-purple-600 focus:text-purple-600">
+              <h3 className="text-2xl font-bold">Balancer Liquidity Pool ⚖️</h3>
+              <p className="mt-4 text-xl">
+                <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ELIMU/$ETH 80%/20%</code>
+              </p>
+              <p className="mt-4">
+                Token emissions: 0.00 <code className="font-mono">$ELIMU</code>/day
+              </p>
+              <p className="mt-2">
+                TVL: 0.00 <code className="font-mono">$ETH</code> &nbsp;&nbsp;&nbsp; APY: 0.00%
+              </p>
+            </a>
+          </div>
 
-          <a href="/balancer" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-purple-600 focus:text-purple-600">
-            <h3 className="text-2xl font-bold">Balancer Liquidity Pool ⚖️</h3>
-            <p className="mt-4 text-xl">
-              <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ELIMU/$ETH 80%/20%</code>
-            </p>
-            <p className="mt-4">
-              APY: 0.00%
-            </p>
-          </a>
-
-          <a href="https://eng.elimu.ai/contributions/aragon-dao" className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-purple-600 focus:text-purple-600">
+          <a href="https://eng.elimu.ai/contributions/aragon-dao" className=" bg-white p-6 mt-6 text-left border w-96 rounded-xl hover:text-purple-600 focus:text-purple-600">
             <h3 className="font-bold">What is <code>$ELIMU</code>? 💎</h3>
             <p>
-              <code className="font-mono">$ELIMU</code> is the governance token used by the elimu.ai 
+              <a className="text-purple-600" href="https://etherscan.io/token/0xe29797910d413281d2821d5d9a989262c8121cc2">
+                <code className="font-mono">$ELIMU</code>
+              </a> is the governance token used by the elimu.ai 
               Community DAO. You can learn more at&nbsp;
               <a href="https://eng.elimu.ai/contributions/aragon-dao" className="text-purple-600">elimu.ai</a>
               &nbsp;and <a href="https://github.com/elimu-ai/wiki#donate-cryptocurrency-eth" className="text-purple-600">https://github.com/elimu-ai/wiki</a>.
@@ -63,7 +80,7 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <footer className="flex items-center justify-center w-full h-24 border-t mt-10">
         Made with 💜 by&nbsp;<a href="https://eng.elimu.ai" className="text-purple-600">elimu.ai</a>
       </footer>
     </div>

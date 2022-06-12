114460ef21169d1d489d3feb2b942aab6ad1ec4b
Bender-22 <85234598+Bender-22@users.noreply.github.com>
2022-06-12 12:45:32 +0800

    Add deposit buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home() {
             <a href="/uniswap" className="hover:text-purple-600 focus:text-purple-600">
               <h3 className="text-2xl font-bold">Uniswap Liquidity Pool 🦄</h3>
               <p className="mt-4 text-xl">
-                <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ELIMU/$ETH 50%/50%</code>
+                <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ETH/$ELIMU 50%/50%</code>
               </p>
               <p className="mt-4">
                 Token emissions: 0.00 <code className="font-mono">$ELIMU</code>/day
@@ -33,6 +33,9 @@ export default function Home() {
               <p className="mt-2">
                 TVL: 0.00 <code className="font-mono">$ETH</code> &nbsp;&nbsp;&nbsp; APY: 0.00%
               </p>
+            </a>
+            <a href="/uniswap">
+              <button className="bg-purple-500 hover:bg-purple-600 text-white rounded-full mt-4 p-4">Deposit UNI-V2 pool tokens</button>
             </a>
           </div>
 
@@ -40,7 +43,7 @@ export default function Home() {
             <a href="/sushiswap" className="hover:text-purple-600 focus:text-purple-600">
               <h3 className="text-2xl font-bold">SushiSwap Liquidity Pool 🍣</h3>
               <p className="mt-4 text-xl">
-                <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ELIMU/$ETH 50%/50%</code>
+                <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ETH/$ELIMU 50%/50%</code>
               </p>
               <p className="mt-4">
                 Token emissions: 0.00 <code className="font-mono">$ELIMU</code>/day
@@ -48,6 +51,9 @@ export default function Home() {
               <p className="mt-2">
                 TVL: 0.00 <code className="font-mono">$ETH</code> &nbsp;&nbsp;&nbsp; APY: 0.00%
               </p>
+            </a>
+            <a href="/sushiswap">
+              <button className="bg-purple-500 hover:bg-purple-600 text-white rounded-full mt-4 p-4">Deposit SLP pool tokens</button>
             </a>
           </div>
           
@@ -55,7 +61,7 @@ export default function Home() {
             <a href="/balancer" className="hover:text-purple-600 focus:text-purple-600">
               <h3 className="text-2xl font-bold">Balancer Liquidity Pool ⚖️</h3>
               <p className="mt-4 text-xl">
-                <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ELIMU/$ETH 80%/20%</code>
+                <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">$ETH/$ELIMU 20%/80%</code>
               </p>
               <p className="mt-4">
                 Token emissions: 0.00 <code className="font-mono">$ELIMU</code>/day
@@ -64,9 +70,12 @@ export default function Home() {
                 TVL: 0.00 <code className="font-mono">$ETH</code> &nbsp;&nbsp;&nbsp; APY: 0.00%
               </p>
             </a>
+            <a href="/balancer">
+              <button className="bg-purple-500 hover:bg-purple-600 text-white rounded-full mt-4 p-4">Deposit 20WETH-80ELIMU pool tokens</button>
+            </a>
           </div>
 
-          <a href="https://eng.elimu.ai/contributions/aragon-dao" className=" bg-white p-6 mt-6 text-left border w-96 rounded-xl hover:text-purple-600 focus:text-purple-600">
+          <div className=" bg-white p-6 mt-6 border w-96 rounded-2xl text-left">
             <h3 className="font-bold">What is <code>$ELIMU</code>? 💎</h3>
             <p>
               <a className="text-purple-600" href="https://etherscan.io/token/0xe29797910d413281d2821d5d9a989262c8121cc2">
@@ -76,7 +85,7 @@ export default function Home() {
               <a href="https://eng.elimu.ai/contributions/aragon-dao" className="text-purple-600">elimu.ai</a>
               &nbsp;and <a href="https://github.com/elimu-ai/wiki#donate-cryptocurrency-eth" className="text-purple-600">https://github.com/elimu-ai/wiki</a>.
             </p>
-          </a>
+          </div>
         </div>
       </main>
 

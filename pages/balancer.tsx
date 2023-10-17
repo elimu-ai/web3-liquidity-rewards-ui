3cad3078da6fc03ac1db4de48534f134f31fa6a3
Nya Ξlimu <info@elimu.ai>
2023-10-18 01:06:59 +0700

    Update balancer.tsx

--- pages/balancer.tsx
+++ pages/balancer.tsx
@@ -43,7 +43,7 @@ export default function Balancer() {
               <div className='p-6'>
                 <h3 className="text-2xl font-bold">1. Provide Liquidity 💧</h3>
                 <p className='mt-4'>
-                  Provide liquidity to the <a className="font-bold text-purple-600" target="_blank" rel="noreferrer" href="https://app.balancer.fi/#/ethereum/swap/ether/0xe29797910D413281d2821D5d9a989262c8121CC2">Balancer pool</a>
+                  Provide liquidity to the <a className="font-bold text-purple-600" target="_blank" rel="noreferrer" href="https://app.balancer.fi/#/ethereum/pool/0x517390b2b806cb62f20ad340de6d98b2a8f17f2b0002000000000000000001ba">Balancer pool</a>
                   , and receive Balancer pool tokens.
                 </p>
               </div>

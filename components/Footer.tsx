e445183a00944a7bf952fa2a69ba886976c616ed
Tomar Abhay <tomarabhay363@gmail.com>
2025-11-01 16:36:03 +0530

    Fix: add GitHub repo link to footer (closes #136)

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,3 @@
-// export default function Footer() {
-//   return (
-//     <>
-//       <footer className="flex items-center justify-center w-full h-24 border-t mt-10">
-//         Made with 💜 by&nbsp;<a href="https://elimu.ai" className="text-purple-600">elimu.ai</a>
-//       </footer>
-//     </>
-//   )
-// }
-
-
-
-
-
-// import React from "react";
-
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
